b99e9d5315a34dc4eae5cd9e57f9b85e22597bdd
Shivam Mishra <scm.mymail@gmail.com>
2020-10-03 20:20:13 +0530

    feat: setup tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+	future: {
+		removeDeprecatedGapUtilities: true,
+		purgeLayersByDefault: true,
+	},
+	purge: [],
+	theme: {
+		extend: {},
+		colors: {
+			'white': '#FFFFFF',
+			'black': '#000000',
+			'mighty-teal': '#174057',
+			'timid-teal': '#60D6D7',
+			'panoptic-blue': '#04183D',
+			'orangish': '#E37A30;',
+			'orange': {
+				50: '#FEF7F4',
+				100: '#FCF0EA',
+				200: '#F8D9CA',
+				300: '#F3C1AA',
+				400: '#EB936B',
+				500: '#E2652B',
+				600: '#CB5B27',
+				700: '#883D1A',
+				800: '#662D13',
+				900: '#441E0D',
+			},
+		}
+	},
+	variants: {},
+	plugins: [],
+}
