e3ed9db1f5bc9f02e2cbb6245091d03bd4e48a70
Shivam Mishra <scm.mymail@gmail.com>
2020-10-11 16:21:22 +0530

    feat: add state wise count for map

--- panoptic/www/index.py
+++ panoptic/www/index.py
@@ -3,4 +3,21 @@ import frappe
 def get_context(context):
 	context.total_frt = frappe.db.count("FRT")
 	context.total_frs = frappe.db.count("Facial Recognition System")
+	context.state_wise_frt = get_state_wise_frt()
 	context.total_authorities = frappe.db.count("Authority")
+
+def get_state_wise_frt():
+	data = frappe.db.sql("""
+		SELECT
+			`st`.`state_id`,
+			count(`frt`.`name`) as count
+		FROM
+			`tabFRT` as `frt`,
+			`tabState` as `st`
+		WHERE
+			`frt`.`state`=`st`.`name`
+		GROUP BY
+			`frt`.`state`
+	""", as_dict=1)
+
+	return {d.state_id:d.count for d in data}
