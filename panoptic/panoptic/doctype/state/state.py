9f0f892d8313cf393ac51d3c490535f1e7c68718
Shivam Mishra <scm.mymail@gmail.com>
2020-10-18 20:14:15 +0530

    feat: state as a page

--- panoptic/panoptic/doctype/state/state.py
+++ panoptic/panoptic/doctype/state/state.py
@@ -3,8 +3,43 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
-from frappe.model.document import Document
+import frappe
+from frappe.website.website_generator import WebsiteGenerator
 
-class State(Document):
-	pass
+class State(WebsiteGenerator):
+
+	published = 1
+	website = frappe._dict(
+		page_title_field="state_name",
+		no_cache=1,
+		sitemap=1
+	)
+
+	def get_context(self, context):
+		context.frts = frappe.get_all("FRT", filters={ "state": self.name }, limit=20)
+		context.total_frt = frappe.db.count("FRT", {"state": self.name}) or 0
+		context.total_frt_in_use = frappe.db.count("FRT", {"state": self.name, "status": "In Utilization"}) or 0
+		context.total_cost = sum(frappe.db.get_all("FRT", fields=["amount_spent"], filters={ "state": self.name }, pluck="amount_spent"))
+
+		context.state_wise_frt = get_state_wise_frt()
+		context.state_routes = get_state_route_map()
+
+def get_state_route_map():
+	states = frappe.get_all("State", fields={"state_id", "route"})
+	return {d.state_id:d.route for d in states}
+
+def get_state_wise_frt():
+	data = frappe.db.sql("""
+		SELECT
+			`st`.`state_id`,
+			count(`frt`.`name`) as count
+		FROM
+			`tabFRT` as `frt`,
+			`tabState` as `st`
+		WHERE
+			`frt`.`state`=`st`.`name`
+		GROUP BY
+			`frt`.`state`
+	""", as_dict=1)
+
+	return {d.state_id:d.count for d in data}
