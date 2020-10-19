40e284ee7175061a32b5ee996a614c8fa9f90121
Shivam Mishra <scm.mymail@gmail.com>
2020-10-19 11:52:02 +0530

    feat: kickstart frt page

--- panoptic/panoptic/doctype/frt/frt.py
+++ panoptic/panoptic/doctype/frt/frt.py
@@ -3,8 +3,13 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
-from frappe.model.document import Document
+import frappe
+from frappe.website.website_generator import WebsiteGenerator
 
-class FRT(Document):
-	pass
+class FRT(WebsiteGenerator):
+	def on_update(self):
+		self.route = self.make_route()
+
+	def make_route(self):
+		return frappe.get_value("State", self.state, 'route') + '/' + self.name
+
