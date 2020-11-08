599ae47262fc48bf0b409a342b8930f6a8dd1d92
Shivam Mishra <scm.mymail@gmail.com>
2020-11-08 16:01:25 +0530

    feat: add panoptic settings

---
+++ panoptic/panoptic/api.py
@@ -0,0 +1,24 @@
+import frappe
+
+def update_website_context(context):
+	context.settings = frappe.get_doc("Panoptic Settings")
+
+def get_state_route_map(field="state_id"):
+	states = frappe.get_all("State", fields={field, "route"})
+	return {d[field]:d.route for d in states}
+
+def get_state_wise_frt():
+	data = frappe.db.sql("""
+		SELECT
+			`st`.`state_id`,
+			count(`frt`.`name`) as count
+		FROM
+			`tabFRT` as `frt`,
+			`tabState` as `st`
+		WHERE
+			`frt`.`state`=`st`.`name`
+		GROUP BY
+			`frt`.`state`
+	""", as_dict=1)
+
+	return {d.state_id:d.count for d in data}
