52bd7024c0a58b7013b4cdc01469101dca116a53
Shivam Mishra <scm.mymail@gmail.com>
2020-11-16 18:11:13 +0530

    fix: currency formatting error

--- panoptic/utils.py
+++ panoptic/utils.py
@@ -1,10 +1,14 @@
+import frappe
+
 def shorten_number(number):
 	number_system = get_number_system()
 	for system in number_system:
 		if number >= system.get('divisor'):
-			return "₹ {0} {1}".format(round((number/system.get('divisor')), 2), system.get('symbol'))
+			number = number/system.get('divisor')
+			number = frappe.utils.fmt_money(number, 2, "INR")
+			return "{0} {1}".format(number, system.get('symbol'))
 
-	return round(number, 2);
+	return frappe.utils.fmt_money(number, 0, "INR")
 
 def get_number_system():
 	return [
