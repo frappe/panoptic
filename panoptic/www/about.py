94e3b14edbd7cc15ae7faefe477f9208a1a729e3
Shivam Mishra <scm.mymail@gmail.com>
2020-11-24 15:13:17 +0530

    chore: minor templating updates

--- panoptic/www/about.py
+++ panoptic/www/about.py
@@ -3,4 +3,4 @@ import frappe
 def get_context(context):
 	context.no_cache = 1
 	context.about = frappe.get_doc("About Page")
-	context.team = frappe.get_all("Team Member", filters={"show_on_website": 1}, fields=["*"])
+	context.team = frappe.get_all("Team Member", filters={"show_on_website": 1}, fields=["*"], order_by="full_name desc")
