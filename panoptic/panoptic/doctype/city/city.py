bd08fd47c57fad66d42f7e1077ecb5b9590828df
Shivam Mishra <scm.mymail@gmail.com>
2022-05-02 12:16:45 +0530

    chore: update metadata

--- panoptic/panoptic/doctype/city/city.py
+++ panoptic/panoptic/doctype/city/city.py
@@ -7,3 +7,11 @@ from frappe.website.website_generator import WebsiteGenerator
 class City(WebsiteGenerator):
 	def make_route(self):
 		return 'cities/' + self.scrub(self.title)
+
+	def get_context(self, context):
+		context.metatags = {
+			"name": "Panoptic: {0}".format(self.title),
+			"description": self.description,
+			"image": "/assets/panoptic/images/meta/cities.png",
+			"og:type": "article"
+		}
